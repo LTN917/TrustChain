6512c98e1bb8834b1137e88a01c07a7d83a7e6aa
Bryan <lcl25k10@gmail.com>
2024-05-04 13:59:53 +0800

    2024-05-04 setting batch reading queue API

--- src/pages/api/MQ/system_connection.ts
+++ src/pages/api/MQ/system_connection.ts
@@ -1,27 +1,108 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { connect } from 'amqplib';
 
+// queue setting
+let connection:any;
+let channel:any;
+let queue = 'platform_data_queue';
+
+// consumer setting
+let consumer_number = 1;
+let max_consumer_number = 5;
+let intervalId:NodeJS.Timeout;
+
+// start RabbitMQ queue service
+async function initRabbitMQ(){
+  try{
+    if(!connection){
+      connection = await connect('amqp://localhost:5276');
+      channel = await connection.createChannel();
+      await channel.assertQueue(queue, {durable:true})
+      await startConsumer();
+      intervalId=setInterval(adjustConsumers, 60000); // adjust consumers
+    }
+  }catch (err){
+    console.log(`Fail to init RabbitMQ: ${err}`)
+  }
+}
+
+// finish RabbitMQ queue service
+async function finRabbitMQ(){
+  clearInterval(intervalId);
+  try {
+    if (channel) {
+      await channel.close();
+      channel = null;  // Nullify the global reference
+    }
+    if (connection) {
+      await connection.close();
+      connection = null;  // Nullify the global reference
+    }
+  }catch(err){
+    console.log(`Fail to close RabbitMQ: ${err}`)
+  }
+}
+
+// start a consumer 
+async function startConsumer(){
+  try{
+    const localChannel = await connection.createChannel();
+    await localChannel.prefetch(1); // fair dispatch
+    await localChannel.consume(queue,(msg:any)=>{
+      if(msg){
+        console.log('Received:', msg.content.toString());
+        await api_processing_data(msg.content.toString()); // API - processing after receiving the data
+        localChannel.ack(msg);
+      }
+    });
+  }catch(err){
+    console.log(`Fail to start consumer: ${err}`)
+  }
+}
+
+// API - processing after consumer receiving data
+import axios from 'axios';
+async function api_processing_data(msgContent:string){
+  try{
+    const data = JSON.parse(msgContent);
+    const response = await axios.post('http://localhost:3000/api/blockchain/upto_blockchain',data);
+    console.log(`API Response: ${response.Info}`) // return the result of processing
+  }catch(err){
+    console.log(`Consumer Fail to use processing data API: ${err}`);
+  }
+}
+
+// adjust number of consumers
+async function adjustConsumers(){
+  try{
+    const { messageCount } = await channel.checkQueue(queue);
+    while(messageCount > 100 && consumer_number < max_consumer_number){
+      await startConsumer();
+      consumer_number++;
+    }  
+  }catch(err){
+    console.log(`Fail to adjust Consumer: ${err}`);
+  }
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
 
-    // create rabbitMQ connection
-    const connection = await connect('amqp://localhost:5276');
-    const channel = await connection.createChannel();
-    const queue = 'platform_data_queue';
-
-    // assure MQ exist
-    await channel.assertQueue(queue, { durable: true });
-
-    // send platform data to queue
-    channel.sendToQueue(queue, Buffer.from(JSON.stringify(req.body)), { persistent: true });
-    
-    console.log('Data sent to queue:', req.body);
-    await channel.close();
-    await connection.close();
-
-    res.status(200).json({ message: 'platform dataset send to queue' });
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  try{
+    await initRabbitMQ();
+    if (req.method === 'POST') {
+      // platform send data to queue
+      await channel.sendToQueue(queue, Buffer.from(JSON.stringify(req.body)), { persistent: true });
+      console.log('Data sent to queue:', req.body);
+      res.status(200).json({ message: 'platform dataset send to queue' });
+    } else {
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+    // await finRabbitMQ();
+  }catch(err){
+    console.error(`API request failed: ${err}`);
+    res.status(500).json({ error: 'Internal server error' });
+  }finally{
+    await finRabbitMQ();
   }
 }
