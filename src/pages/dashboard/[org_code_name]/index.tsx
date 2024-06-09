e1899677607289f0857e4b2fb62cd97b705c57e0
Brayn Lai <lcl25k10@gmail.com>
2024-06-10 00:25:13 +0800

    2024-06-09 dashboard FE [done]

--- src/pages/dashboard/[org_code_name]/index.tsx
+++ src/pages/dashboard/[org_code_name]/index.tsx
@@ -1,29 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import Navbar from '../../components/Navbar'; // Update the import path as necessary
+import Navbar from '../../components/Navbar';
+import styles from '../../../styles/dashboard.module.css';
 
-const Home = () => {
+const Dashboard = () => {
     const router = useRouter();
-    const { org_code_name } = router.query;  // 从 URL 获取 username
-    const isAuthenticated = true; // This should be derived from your auth logic
+    const { org_code_name } = router.query;
+    const isAuthenticated = true;
+    const [activeTab, setActiveTab] = useState('upto_blockchain');
+
+    const handleTabChange = (tab) => {
+        setActiveTab(tab);
+    };
 
     return (
         <>
             <Navbar isAuthenticated={isAuthenticated} userName={org_code_name as string} />
-            <h1>Dashboard</h1>
-            {/* <div className={styles.background}></div>
-            <div className={styles.main}>
-                <h1 className={styles.fadeInElement}>Welcome to TrustChain</h1>
-                <p className={styles.fadeInElement}>Explore our blockchain-driven system, designed for the secure and traceable in your shared platforms</p>
-                <button className={styles.button}>
-                    <a href="/learn-more">Learn More</a>
-                </button>
-                <button className={styles.button}>
-                    <a href="/signup">Sign up</a>
-                </button>
-            </div> */}
+            <div className={styles.dashboard}>
+                <h1 className={styles.dashboardTitle}>Dashboard</h1>
+                <div className={styles.tabContainer}>
+                    <button className={`${styles.tabButton} ${activeTab === 'upto_blockchain' ? styles.active : ''}`}
+                            onClick={() => handleTabChange('upto_blockchain')}>
+                        Data Up to Blockchain
+                    </button>
+                    <button className={`${styles.tabButton} ${activeTab === 'verify_rp' ? styles.active : ''}`}
+                            onClick={() => handleTabChange('verify_rp')}>
+                        Verify RP on Blockchain
+                    </button>
+                </div>
+                <div className={styles.content}>
+                    {activeTab === 'upto_blockchain' && (
+                        <div className={styles.form}>
+                            <label htmlFor="jsonData">Authorization Data Entry JSON</label>
+                            <textarea id="jsonData" placeholder="Enter Authorization Data JSON" className={styles.textarea}></textarea>
+                            <label htmlFor="fileUpload" className={styles.fileUploadLabel}>Upload Authdata File</label>
+                            <input id="fileUpload" type="file" className={styles.fileUploadInput}/>
+                            <button className={styles.actionButton}>Send to Blockchain</button>
+                        </div>
+                    )}
+                    {activeTab === 'verify_rp' && (
+                        <div className={styles.form}>
+                            <label htmlFor="jsonData">Request Party Info JSON</label>
+                            <textarea id="jsonData" placeholder="Enter Authorization Data JSON" className={styles.textarea}></textarea>
+                            <label htmlFor="fileUpload" className={styles.fileUploadLabel}>Upload RP Info File</label>
+                            <input id="fileUpload" type="file" className={styles.fileUploadInput}/>
+                            <button className={styles.actionButton}>Verify RP</button>                        </div>
+                    )}
+                </div>
+            </div>
         </>
     );
 };
 
-export default Home;
+export default Dashboard;
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -36,7 +36,7 @@ const Signup: React.FC = () => {
             setMessage({ type: 'success', content: 'Registration successful! Redirecting to home...' });
             const id = setTimeout(() => {
                 router.push('/');
-            }, 5000); // Redirect after 3 seconds
+            }, 3000); // Redirect after 3 seconds
             setTimeoutId(id); // Store the timeout ID for potential cleanup
         } catch (error: any) {
             console.error(error);
